ef175ebfb80e92caf3a8de88d479e6c4aa13ce74
Lucas <lucaaszstr@gmail.com>
2025-06-27 14:09:58 -0300

    Exercicio de tabuada com for

---
+++ loop/Tabuada.java
@@ -0,0 +1,19 @@
+package loop;
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+
+        int numero = scanner.nextInt();
+        
+        for (int i = 1; i < 11; i++){
+            System.out.println(numero + " * " + i + " = " + numero * i);
+        }
+
+
+        scanner.close();
+    }
+}
